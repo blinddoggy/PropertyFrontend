5d8b5c42cebf2d41272b0578d61814301ecb9d02
paladines4653 <paladines4653@gmail.com>
2023-06-05 18:23:12 -0500

    update middleware to use url instead nextUrl

--- middleware.ts
+++ middleware.ts
@@ -2,20 +2,21 @@ import { NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
 export async function middleware(request: any) {
-	const jwt = request.cookies.get('ProfileJWT');
-	const url = request.nextUrl.pathname;
+	const jwt = request.cookies?.get('ProfileJWT');
+	const url = request.url;
 	const secret = new TextEncoder().encode(process.env.SECRET);
-	if (!secret) NextResponse.redirect(new URL('/notfound.env', request.url));
+
+	if (!secret) return NextResponse.redirect(new URL('/notfound.env', url));
 
 	if (!url.startsWith('/login') && !jwt) {
-		return NextResponse.redirect(new URL('/login', request.url));
+		return NextResponse.redirect(new URL('/login', url));
 	}
 
-	if (request.nextUrl.pathname.startsWith('/login')) {
+	if (url.startsWith('/login')) {
 		if (jwt && secret) {
 			try {
 				await jwtVerify(jwt.value, secret);
-				return NextResponse.redirect(new URL('/projects', request.url));
+				return NextResponse.redirect(new URL('/projects', url));
 			} catch (error) {
 				return NextResponse.next();
 			}
@@ -23,16 +24,16 @@ export async function middleware(request: any) {
 		return NextResponse.next();
 	}
 
-	if (request.nextUrl.pathname.startsWith('/projects')) {
+	if (url.startsWith('/projects')) {
 		try {
 			if (jwt && secret) {
 				await jwtVerify(jwt.value, secret);
 				return NextResponse.next();
 			}
 		} catch (error) {
-			return NextResponse.redirect(new URL('/login', request.url));
+			return NextResponse.redirect(new URL('/login', url));
 		}
-		return NextResponse.redirect(new URL('/login', request.url));
+		return NextResponse.redirect(new URL('/login', url));
 	}
 }
 
