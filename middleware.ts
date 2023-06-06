385edfe877e664b81b65a24e336a68b816f85140
paladines4653 <paladines4653@gmail.com>
2023-06-05 21:18:23 -0500

    hotfix: TurnOff middleware

--- middleware.ts
+++ middleware.ts
@@ -2,35 +2,35 @@ import { NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
 export async function middleware(request: any) {
-	const jwt = request.cookies?.get('ProfileJWT');
-	const url = request.url;
-	const secret = new TextEncoder().encode(process.env.SECRET) || '';
+	// const jwt = request.cookies?.get('ProfileJWT');
+	// const url = request.url;
+	// const secret = new TextEncoder().encode(process.env.SECRET) || '';
 
-	if (!jwt) {
-		if (url !== '/login') {
-			return NextResponse.redirect(new URL('/login', url));
-		} else {
-			return NextResponse.next();
-		}
-	}
+	// if (!jwt) {
+	// 	if (url !== '/login') {
+	// 		return NextResponse.redirect(new URL('/login', url));
+	// 	} else {
+	// 		return NextResponse.next();
+	// 	}
+	// }
 
-	if (url.startsWith('/login')) {
-		try {
-			await jwtVerify(jwt.value, secret);
-			return NextResponse.redirect(new URL('/projects', url));
-		} catch (error) {
-			return NextResponse.next();
-		}
-	}
+	// if (url.startsWith('/login')) {
+	// 	try {
+	// 		await jwtVerify(jwt.value, secret);
+	// 		return NextResponse.redirect(new URL('/projects', url));
+	// 	} catch (error) {
+	// 		return NextResponse.next();
+	// 	}
+	// }
 
-	if (url.startsWith('/projects')) {
-		try {
-			await jwtVerify(jwt.value, secret);
-			return NextResponse.next();
-		} catch (error) {
-			return NextResponse.redirect(new URL('/login', url));
-		}
-	}
+	// if (url.startsWith('/projects')) {
+	// 	try {
+	// 		await jwtVerify(jwt.value, secret);
+	// 		return NextResponse.next();
+	// 	} catch (error) {
+	// 		return NextResponse.redirect(new URL('/login', url));
+	// 	}
+	// }
 
 	return NextResponse.next();
 }
